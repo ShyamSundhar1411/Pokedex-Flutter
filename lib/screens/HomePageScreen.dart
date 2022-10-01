9e1b61489e25e401b96ec815430a14826520b8f0
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2022-10-01 20:10:02 +0530

    Flutter

--- lib/screens/HomePageScreen.dart
+++ lib/screens/HomePageScreen.dart
@@ -21,25 +21,8 @@ class MyHomePage extends StatelessWidget {
           title: Center(
             child: Text("Pokedex"),
           ),
+          backgroundColor: Colors.deepOrangeAccent,
           flexibleSpace: FlexibleSpaceBar(
-            background: Stack(
-              fit: StackFit.expand,
-              children: [
-                DecoratedBox(
-                    decoration: BoxDecoration(
-                        border: Border.all(width: 5, color: Colors.transparent),
-                        borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(30),
-                            bottomRight: Radius.circular(30)),
-                        gradient: LinearGradient(
-                            colors: [
-                              Color(0xFFfccb90),
-                              Color(0xFFd57eeb),
-                            ],
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight)))
-              ],
-            ),
             stretchModes: <StretchMode>[
               StretchMode.zoomBackground,
               StretchMode.blurBackground,
